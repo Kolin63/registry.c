a34abc7a5de18a9e68a31f7f96a1fcfb293880b7
Kolin63 <theverylastgoodname@gmail.com>
2026-04-01 21:55:41 -0400

    Add Unit Test

--- tests/test.c
+++ tests/test.c
@@ -1,4 +1,3 @@
-#include <assert.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,95 +5,108 @@
 
 #include "../src/registry.h"
 
-#define KEY_CHECK(reg, i, key) \
-  if (strcmp(reg->keys[i], key) != 0) assert(key && false);
+static int tests_passed = 0;
+static int tests_total = 0;
 
-struct animal {
-  int age;
-  char* name;
-  char* noise;
-};
+#define KEY_CHECK(reg, i, key)                                               \
+  tests_total++;                                                             \
+  if (strcmp(reg->keys[i], key) == 0) {                                      \
+    printf("\e[0;92m%s:%i: Correct key: %s\e[0m\n", __FILE_NAME__, __LINE__, \
+           key);                                                             \
+    tests_passed++;                                                          \
+  } else                                                                     \
+    printf("\e[0;91m%s:%i: Incorrect key. Got: %s. Expected: %s\e[0m\n",     \
+           __FILE_NAME__, __LINE__, reg->keys[i], key);
+
+#define VALUE_CHECK(reg, i, val_type, val)                                  \
+  tests_total++;                                                            \
+  {                                                                         \
+    const val_type val_buf = val;                                           \
+    if (memcmp(&val_buf, reg->values + i * reg->val_size, reg->val_size) == \
+        0) {                                                                \
+      printf("\e[0;92m%s:%i: Correct value: %s\e[0m\n", __FILE_NAME__,      \
+             __LINE__, #val);                                               \
+      tests_passed++;                                                       \
+    } else {                                                                \
+      printf("\e[0;91m%s:%i: Incorrect value. Expected: %s\e[0m\n",         \
+             __FILE_NAME__, __LINE__, #val);                                \
+    }                                                                       \
+  }
+
+#define KEY_VALUE_CHECK(reg, i, key, val_type, val) \
+  KEY_CHECK(reg, i, key);                           \
+  VALUE_CHECK(reg, i, val_type, val);
 
 void animal_test() {
-  struct registry* reg = registry_init(sizeof(struct animal));
+  struct registry* reg = registry_init(sizeof(int));
 
   {
-    char* key = malloc(6);
-    strcpy(key, "willy");
-    char* name = malloc(6);
-    strcpy(name, "Willy");
-    char* noise = malloc(6);
-    strcpy(noise, "Woof!");
-
-    struct animal x = {.age = 6, .name = name, .noise = noise};
-    registry_add(reg, key, &x);
+    char* key = "willy";
+    int age = 6;
+    registry_add(reg, key, &age);
   }
 
-  {
-    char* key = malloc(9);
-    strcpy(key, "twilight");
-    char* name = malloc(9);
-    strcpy(name, "Twilight");
-    char* noise = malloc(6);
-    strcpy(noise, "Meow!");
+  KEY_VALUE_CHECK(reg, 0, "willy", int, 6);
 
-    struct animal x = {.age = 8, .name = name, .noise = noise};
-    registry_add(reg, key, &x);
+  {
+    char* key = "twilight";
+    int age = 8;
+    registry_add(reg, key, &age);
   }
 
-  {
-    char* key = malloc(11);
-    strcpy(key, "apple_jack");
-    char* name = malloc(11);
-    strcpy(name, "Apple Jack");
-    char* noise = malloc(6);
-    strcpy(noise, "Neigh");
+  KEY_VALUE_CHECK(reg, 0, "twilight", int, 8);
+  KEY_VALUE_CHECK(reg, 1, "willy", int, 6);
 
-    struct animal x = {.age = 28, .name = name, .noise = noise};
-    registry_add(reg, key, &x);
-    registry_add(reg, key, &x);
+  {
+    char* key = "apple_jack";
+    int age = 28;
+    registry_add(reg, key, &age);
+    registry_add(reg, key, &age);
   }
 
-  {
-    char* key = malloc(5);
-    strcpy(key, "puff");
-    char* name = malloc(5);
-    strcpy(name, "Puff");
-    char* noise = malloc(5);
-    strcpy(noise, "Roar");
+  KEY_VALUE_CHECK(reg, 0, "apple_jack", int, 28);
+  KEY_VALUE_CHECK(reg, 1, "twilight", int, 8);
+  KEY_VALUE_CHECK(reg, 2, "willy", int, 6);
 
-    struct animal x = {.age = 20, .name = name, .noise = noise};
-    registry_add(reg, key, &x);
+  {
+    char* key = "puff";
+    int age = 23;
+    registry_add(reg, key, &age);
   }
 
-  {
-    char* key = malloc(5);
-    strcpy(key, "babe");
-    char* name = malloc(5);
-    strcpy(name, "Babe");
-    char* noise = malloc(5);
-    strcpy(noise, "Oink");
+  KEY_VALUE_CHECK(reg, 0, "apple_jack", int, 28);
+  KEY_VALUE_CHECK(reg, 1, "puff", int, 23);
+  KEY_VALUE_CHECK(reg, 2, "twilight", int, 8);
+  KEY_VALUE_CHECK(reg, 3, "willy", int, 6);
 
-    struct animal x = {.age = 10, .name = name, .noise = noise};
-    registry_add(reg, key, &x);
+  {
+    char* key = "babe";
+    int age = 10;
+    registry_add(reg, key, &age);
   }
 
   for (size_t i = 0; i < reg->length; i++) {
-    const struct animal* elem =
-        (struct animal*)((char*)reg->values + i * reg->val_size);
-    printf(
-        "animal test #%zi:'%s':\n"
-        "  age: %i\n"
-        "  name: %s\n"
-        "  noise: %s\n",
-        i, reg->keys[i], elem->age, elem->name, elem->noise);
+    printf("animal_test #%zi, %s, age %i\n", i, reg->keys[i],
+           *(int*)(registry_get_by_index(reg, i)));
   }
 
-  KEY_CHECK(reg, 0, "apple_jack");
-  KEY_CHECK(reg, 1, "babe");
-  KEY_CHECK(reg, 2, "puff");
-  KEY_CHECK(reg, 3, "twilight");
-  KEY_CHECK(reg, 4, "willy");
+  KEY_VALUE_CHECK(reg, 0, "apple_jack", int, 28);
+  KEY_VALUE_CHECK(reg, 1, "babe", int, 10);
+  KEY_VALUE_CHECK(reg, 2, "puff", int, 23);
+  KEY_VALUE_CHECK(reg, 3, "twilight", int, 8);
+  KEY_VALUE_CHECK(reg, 4, "willy", int, 6);
+
+  registry_cleanup(reg);
 }
 
-int main() { animal_test(); }
+int main() {
+  animal_test();
+
+  if (tests_passed == tests_total) {
+    printf("\n\e[0;102m\e[1;30m\e[4;30mAll Tests Passed (%i/%i)\e[0m\n",
+           tests_passed, tests_total);
+  } else {
+    printf("\n\e[0;101m\e[1;30m\e[4;30mFailed Tests (%i/%i)\e[0m\n",
+           tests_passed, tests_total);
+  }
+}
